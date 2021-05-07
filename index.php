a27b342915446c9024ed283507fd6cbe649b8e38
IPLUG <iplug@sac.iiserpune.ac.in>
2021-05-07 09:34:54 +0530

    Make php from HTML for index

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 <head><title>IPLUG (IISER Pune) | Home </title></head>
 <body bgcolor="#FFFF00">
 <h1 align="center"> Welcome to IPLUG </h1>
+  <?php
+     echo "Hello World!";
+  ?>
 </body>
 </html>
